ecb0839d8aa433730a9e64a688943b87ea8c0f65
Douglas Lau (doug.lau@state.mn.us) <Douglas Lau (doug.lau@state.mn.us)>
2012-08-08 15:09:15 -0500

    Added distanceHaversine method for Position distance calculations.

--- src/us/mn/state/dot/geokit/Position.java
+++ src/us/mn/state/dot/geokit/Position.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2009  Minnesota Department of Transportation
+ * Copyright (C) 2009-2012  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,6 +21,9 @@ package us.mn.state.dot.geokit;
  * @author Douglas Lau
  */
 public class Position {
+
+	/** Mean radius of Earth (in meters) */
+	static private final double MEAN_RADIUS = 6371009.0;
 
 	/** Latitude (degrees) */
 	protected final double latitude;
@@ -46,5 +49,24 @@ public class Position {
 			throw new IllegalArgumentException("Invalid longitude");
 		latitude = lat;
 		longitude = lon;
+	}
+
+	/** Calculate the distance to another position (meters).  This method
+	 * uses the Haversine formula to calculate distance.
+	 * @param op Other position.
+	 * @return Distance to other position (meters). */
+	public double distanceHaversine(Position op) {
+		double lat1 = Math.toRadians(latitude);
+		double lat2 = Math.toRadians(op.latitude);
+		double lon1 = Math.toRadians(longitude);
+		double lon2 = Math.toRadians(op.longitude);
+		double dlon = lon2 - lon1;
+		double dlat = lat2 - lat1;
+		double sdlat2 = Math.sin(dlat / 2.0);
+		double coslat = Math.cos(lat1) * Math.cos(lat2);
+		double sdlon2 = Math.sin(dlon / 2.0);
+		double a = sdlat2 * sdlat2 + coslat * sdlon2 * sdlon2;
+		double c = 2.0 * Math.atan2(Math.sqrt(a), Math.sqrt(1.0 - a));
+		return MEAN_RADIUS * c;
 	}
 }
